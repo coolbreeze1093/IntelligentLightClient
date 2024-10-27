5a2da28c7c59d9bc2292be8330a94ded824c3f15
wei <coolbreeze23@163.com>
2024-10-27 20:49:04 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include <ESP32PWM.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include <string>
+
+ESP32PWM m_pwm;
+const char *ssid = "secret";
+const char *password = "dahuang123";
+
+const int revPort = 56696;
+const int sendPort = 56698;
+
+const std::string key_type = "type";
+const std::string rev_type_deviceList = "deviceList";
+const std::string rev_type_lightInfo = "lightInfo";
+const std::string send_type_lightInfo = "lightInfo";
+const std::string send_type_deviceList = "deviceList";
+
+const std::string value_brightness = "brightness";
+const std::string value_model = "model";
+const std::string value_deviceIp = "deviceIp";
+const std::string value_deviceName = "deviceName";
+
+WiFiUDP udp;
+
+void setupWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  WiFi.setAutoReconnect(true);
+  Serial.println("\nConnected to WiFi!");
+}
+
+void handlePacket(char *buffer, int len) {
+  JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, buffer);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  std::string _type = doc[key_type];
+  if (_type == rev_type_lightInfo) {
+    int value = doc[value_brightness];
+    //uint32_t _value = map(value, 0, 100, 0, 1024);
+    m_pwm.write(value);
+    Serial.printf("%s: %d\n", value_brightness.c_str(), value);
+  } else if (_type == rev_type_deviceList) {
+    String jsonData = "{\"key\":\"value\"}";
+    udp.beginPacket(udp.remoteIP(), sendPort);
+    udp.write((uint8_t *)jsonData.c_str(), jsonData.length());
+    udp.endPacket();
+  }
+}
+
+void udpReceiveTask(void *param) {
+  char buffer[255];
+  while (true) {
+    int packetSize = udp.parsePacket();
+    if (packetSize > 0) {
+      int len = udp.read(buffer, sizeof(buffer) - 1);
+      if (len > 0) {
+        buffer[len] = '\0';
+        Serial.printf("Received packet from %s:%d\n", udp.remoteIP().toString().c_str(), udp.remotePort());
+        handlePacket(buffer, len);
+      }
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+
+  m_pwm.attachPin(13, 10000, 10);
+  udp.begin(revPort);
+
+  for (size_t i = 0; i < 2; i++) {
+    m_pwm.write(5);
+    delay(300);
+    m_pwm.write(0);
+    delay(300);
+  }
+
+  xTaskCreatePinnedToCore(udpReceiveTask, "UDP Receive Task", 4096, NULL, 1, NULL, 1);
+}
+
+void loop() {
+  // 其他代码
+}
