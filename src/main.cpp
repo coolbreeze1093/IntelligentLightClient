43ef6f76071aaf2b282e1ab32394548744409031
wei <coolbreeze23@163.com>
2024-10-29 23:03:45 +0800

    增加设备回复和亮度回复功能

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,9 @@ const std::string rev_type_deviceList = "deviceList";
 const std::string rev_type_lightInfo = "lightInfo";
 const std::string send_type_lightInfo = "lightInfo";
 const std::string send_type_deviceList = "deviceList";
+const std::string send_type_querylightInfo = "querylightInfo";
 
+const std::string value_localip = "localip";
 const std::string value_brightness = "brightness";
 const std::string value_model = "model";
 const std::string value_deviceIp = "deviceIp";
@@ -25,10 +27,14 @@ const std::string value_deviceName = "deviceName";
 
 WiFiUDP udp;
 
-void setupWiFi() {
+int brightness=0;
+
+void setupWiFi()
+{
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -36,36 +42,74 @@ void setupWiFi() {
   Serial.println("\nConnected to WiFi!");
 }
 
-void handlePacket(char *buffer, int len) {
+void handlePacket(char *buffer, int len)
+{
   JsonDocument doc;
   DeserializationError error = deserializeJson(doc, buffer);
-  if (error) {
+  if (error)
+  {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
   }
 
   std::string _type = doc[key_type];
-  if (_type == rev_type_lightInfo) {
+  if (_type == rev_type_lightInfo)
+  {
     int value = doc[value_brightness];
-    //uint32_t _value = map(value, 0, 100, 0, 1024);
+    // uint32_t _value = map(value, 0, 100, 0, 1024);
+    brightness=value;
     m_pwm.write(value);
     Serial.printf("%s: %d\n", value_brightness.c_str(), value);
-  } else if (_type == rev_type_deviceList) {
-    String jsonData = "{\"key\":\"value\"}";
-    udp.beginPacket(udp.remoteIP(), sendPort);
+  }
+  else if (_type == rev_type_deviceList)
+  {
+    JsonDocument _senddoc;
+    _senddoc[key_type] = send_type_deviceList;
+    _senddoc[value_deviceName] = "flower";
+    _senddoc[value_deviceIp] = WiFi.localIP().toString();
+
+    String _remotip = doc[value_localip];
+
+    IPAddress _ipa;
+    _ipa.fromString(_remotip.c_str());
+    String jsonData;
+    serializeJson(_senddoc, jsonData);
+
+    udp.beginPacket(_ipa, sendPort);
+    udp.write((uint8_t *)jsonData.c_str(), jsonData.length());
+    udp.endPacket();
+  }
+  else if(_type == send_type_querylightInfo)
+  {
+    JsonDocument _senddoc;
+    _senddoc[key_type] = send_type_lightInfo;
+    _senddoc[value_brightness] = brightness;
+
+    String _remotip = doc[value_localip];
+
+    IPAddress _ipa;
+    _ipa.fromString(_remotip.c_str());
+    String jsonData;
+    serializeJson(_senddoc, jsonData);
+
+    udp.beginPacket(_ipa, sendPort);
     udp.write((uint8_t *)jsonData.c_str(), jsonData.length());
     udp.endPacket();
   }
 }
 
-void udpReceiveTask(void *param) {
+void udpReceiveTask(void *param)
+{
   char buffer[255];
-  while (true) {
+  while (true)
+  {
     int packetSize = udp.parsePacket();
-    if (packetSize > 0) {
+    if (packetSize > 0)
+    {
       int len = udp.read(buffer, sizeof(buffer) - 1);
-      if (len > 0) {
+      if (len > 0)
+      {
         buffer[len] = '\0';
         Serial.printf("Received packet from %s:%d\n", udp.remoteIP().toString().c_str(), udp.remotePort());
         handlePacket(buffer, len);
@@ -75,7 +119,8 @@ void udpReceiveTask(void *param) {
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   setupWiFi();
 
@@ -87,7 +132,8 @@ void setup() {
   m_pwm.attachPin(13, 10000, 10);
   udp.begin(revPort);
 
-  for (size_t i = 0; i < 2; i++) {
+  for (size_t i = 0; i < 2; i++)
+  {
     m_pwm.write(5);
     delay(300);
     m_pwm.write(0);
@@ -97,6 +143,8 @@ void setup() {
   xTaskCreatePinnedToCore(udpReceiveTask, "UDP Receive Task", 4096, NULL, 1, NULL, 1);
 }
 
-void loop() {
+void loop()
+{
   // 其他代码
+  delay(500);
 }
